b3a8a8daebc02f45b4eed699c5d454d3a8691898
ugodike <dykeehenry@gmail.com>
2022-05-21 19:41:05 +0100

    update

--- src/route.ts
+++ src/route.ts
@@ -3,4 +3,6 @@ import {Express, Request, Response} from 'express';
 
 export default function(app: Express){
     app.get('/healthcheck',(req:Request, res:Response) => res.sendStatus(200))
+
+    app.post()
 }
